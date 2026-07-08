34d06da98e0469ae30c4a3a666d33452d535dacc
Mano784 <manonithsakthi@gmail.com>
2026-07-08 12:55:22 -0400

    Update main.py

--- main.py
+++ main.py
@@ -17,9 +17,9 @@ birthday_dict = {(data_row["month"],data_row["day"]) : data_row for (index,data_
 
 names = data["name"].to_list()
 files = ["letter_1.txt","letter_2.txt","letter_3.txt"]
-random_file = random.choice(files)
-for _ in range(5):
-    if today_tuple in birthday_dict:
+
+if today_tuple in birthday_dict:
+        random_file = random.choice(files)
         birthday_person = birthday_dict[today_tuple]
         with open(f"letter_templates/{random_file}", "r") as file:
             read_file = file.read()
@@ -28,10 +28,12 @@ for _ in range(5):
 
         with smtplib.SMTP("smtp.gmail.com") as connection:
             connection.starttls()
-            connection.login(my_email,password)
-            connection.sendmail(from_addr = my_email,to_addrs = birthday_person["email"],
-                                 msg = f"Subject: Happy Birthday!!\n\n{updated_content}")
-
+            connection.login(MY_EMAIL, MY_PASSWORD)
+            connection.sendmail(
+                    from_addr=MY_EMAIL,
+                    to_addrs=birthday_person["email"],
+                    msg=f"Subject: Happy Birthday!!\n\n{updated_content}"
+                    )
 
 
 
